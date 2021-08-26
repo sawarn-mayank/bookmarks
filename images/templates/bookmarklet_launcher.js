928d7e94ae923438a74a38089b5ddf7dca55d17c
Mayank Sawarn <mayank.sawarn88@gmail.com>
2021-08-27 02:48:00 +0530

    Initial commit

---
+++ images/templates/bookmarklet_launcher.js
@@ -0,0 +1,10 @@
+(function(){
+	if (window.myBookmarklet !== undefined){
+		myBookmarklet();
+	}
+	else {
+
+document.body.appendChild(document.createElement('script')).src='https://491a-2409-4071-4e92-2399-e1c0-592-c76-e45a.ngrok.io/static/js/bookmarklet.js?
+r='+Math.floor(Math.random()*99999999999999999999);
+	}
+})();
